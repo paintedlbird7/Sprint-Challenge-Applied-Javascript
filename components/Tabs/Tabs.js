6e93990bdcdbe7628d2ce238be35533308c0a45b
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-08 10:20:23 -0800

    Completed MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,7 +11,7 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelector('.card');
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values       
       //this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
